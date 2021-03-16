53a1a388141a65b801f0db36eb94f513a3ad749b
Abas Farah <abasnfarah@gmail.com>
2021-03-16 13:22:01 -0500

    Automates MacOsx Installation

---
+++ brew.sh
@@ -0,0 +1,50 @@
+#Updating Brew
+brew update
+
+brew install bash
+brew install zsh
+brew install neofetch
+brew install tree
+
+packages=(
+	git
+	npm
+	python3
+	vim
+	wget
+	curl
+	tmux
+    zsh-syntax-highlighting
+    nodejs
+    npm
+    mvn
+)
+
+echo "Installing packages..."
+brew install ${packages[@]}
+
+echo "Installing cask"
+brew install ntfs-3g
+
+echo "Cleaning up"
+brew cleanup
+
+echo "Downloading fonts"
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+brew cask install google-chrome
+
+CASKS=(
+    vlc
+	iterm2
+	sublime-text
+	disk-inventory-x
+    spotify
+    virtualbox
+    discord
+)
+
+echo "Installing Cask apps..."
+brew cask install ${CASKS[@]}
+
+
