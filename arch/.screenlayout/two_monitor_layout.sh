7c210d9d83e7c0bc4f831704e6df7dd57fb5041a
Abas Farah <abasnfarah@gmail.com>
2021-11-05 06:58:11 -0500

    Updates arch dotfiles

---
+++ arch/.screenlayout/two_monitor_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DVI-D-1-0 --off --output HDMI-1-0 --off --output HDMI-1-1 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
