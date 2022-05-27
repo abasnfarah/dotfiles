558a67830821d70418689fbe6c0020b016273dd1
Abas Farah <abasnfarah@gmail.com>
2022-05-27 13:30:47 -0500

    Update local.zsh

--- zsh/local.zsh
+++ zsh/local.zsh
@@ -1,3 +1,9 @@
+eval export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 # Go Enviroment/Path
 # ZSH_DISABLE_COMPFIX=true
 export GOPATH=$HOME/go
