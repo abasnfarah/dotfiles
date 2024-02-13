838f07e42b19c0d068d838b1607ac1c0aa4dc5d4
Abas Farah <abasnfarah@gmail.com>
2024-02-13 04:49:50 -0600

    removes test snippet

--- neovim/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/nvim/lua/plugins/nvim-cmp.lua
@@ -10,22 +10,22 @@ return {
     "saadparwaiz1/cmp_luasnip",
     "rafamadriz/friendly-snippets",
     "onsails/lspkind.nvim",
-    "avneesh0612/react-nextjs-snippets",
+    -- "avneesh0612/react-nextjs-snippets",
   },
   config = function()
     local cmp = require("cmp")
     local ls = require("luasnip")
-    local s = ls.snippet
-    local t = ls.text_node
-    local i = ls.insert_node
-
-    ls.add_snippets("lua", {
-      s("hello", {
-        t("print('Hello, '"),
-        i(1),
-        t("!')"),
-      }),
-    })
+    -- local s = ls.snippet
+    -- local t = ls.text_node
+    -- local i = ls.insert_node
+    --
+    -- ls.add_snippets("lua", {
+    --   s("hello", {
+    --     t("print('Hello, '"),
+    --     i(1),
+    --     t("!')"),
+    --   }),
+    -- })
 
     local lspkind = require("lspkind")
     require("luasnip.loaders.from_vscode").lazy_load()
