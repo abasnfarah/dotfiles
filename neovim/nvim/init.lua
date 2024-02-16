36b86f588b5031164b1502d89d1b495a31b91046
Abas Farah <abasnfarah@gmail.com>
2024-02-16 08:37:51 -0600

    Saves folds on exit and loads them on BufWinEnter

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -3,6 +3,10 @@ require("abas.keymaps")
 require("abas.lazy")
 require("abas.markdown")
 
+-- This saves buffer folds on exit and loads them on open
+vim.cmd("autocmd VimLeave * mkview")
+vim.cmd("autocmd BufWinEnter * silent! loadview")
+
 vim.o.background = "dark"
 vim.cmd([[colorscheme catppuccin]])
 -- vim.cmd([[colorscheme darkplus]])
